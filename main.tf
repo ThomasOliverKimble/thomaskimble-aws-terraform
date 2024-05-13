8d8a6de4a811c2d54c8c3e8cc636556ca6bd6809
ThomasOliverKimble <tokimble@gmail.com>
2024-05-13 17:58:39 +0100

    add subdomain for cerrtificate

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
 resource "aws_route53_zone" "thomaskimble" {
   name = "thomaskimble.com"
 }
+
+variable "domains" {
+    type = "list"
+    default = [
+        "example.com",
+        "*.example.com",
+    ]
+}
+
+resource "aws_acm_certificate" "thomaskimble_certificate" {
+  count             = "${length(var.domains)}"
+  domain_name       = "${element(var.domains, count.index)}"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
