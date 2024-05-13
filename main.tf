a534a5b9d36c9c04c485d4cead1fe212b5dbd59f
ThomasOliverKimble <tokimble@gmail.com>
2024-05-13 17:54:19 +0100

    remove certificate

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
 resource "aws_route53_zone" "thomaskimble" {
   name = "thomaskimble.com"
 }
-
-resource "aws_acm_certificate" "thomaskimble_certificate" {
-  domain_name       = "thomaskimble.com"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
