91863d5cd430dd85ed740c7efd2e73c36d82e83b
ThomasOliverKimble <tokimble@gmail.com>
2024-05-15 19:38:02 +0100

    update resources

---
+++ storage/main.tf
@@ -0,0 +1,58 @@
+resource "aws_s3_bucket" "thomaskimble_bucket" {
+  bucket = "thomaskimble-storage"
+}
+
+resource "aws_s3_bucket_policy" "thomaskimble_bucket_policy" {
+  bucket = aws_s3_bucket.thomaskimble_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Effect    = "Allow"
+        Resource = [
+          "${aws_s3_bucket.thomaskimble_bucket.arn}",
+          "${aws_s3_bucket.thomaskimble_bucket.arn}/*"
+        ]
+      },
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.thomaskimble_bucket_public_access_block]
+}
+
+resource "aws_s3_bucket_acl" "thomaskimble_bucket_acl" {
+  bucket     = aws_s3_bucket.thomaskimble_bucket.id
+  acl        = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.thomaskimble_bucket_acl_ownership]
+}
+
+resource "aws_s3_bucket_ownership_controls" "thomaskimble_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.thomaskimble_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.thomaskimble_bucket_public_access_block]
+}
+
+resource "aws_s3_bucket_public_access_block" "thomaskimble_bucket_public_access_block" {
+  bucket = aws_s3_bucket.thomaskimble_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_cors_configuration" "thomaskimble_bucket_cors_configuration" {
+  bucket = aws_s3_bucket.thomaskimble_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
