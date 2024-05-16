d405c47cbc54c299c759cfef6d58bb3f91e9621f
ThomasOliverKimble <tokimble@gmail.com>
2024-05-16 01:58:42 +0100

    replace access identity with access control

--- cdn/main.tf
+++ cdn/main.tf
@@ -7,12 +7,9 @@ locals {
 # CloudFront Distribution for S3 Bucket
 resource "aws_cloudfront_distribution" "thomaskimble_cloudfront_distribution" {
   origin {
-    domain_name = var.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.default.cloudfront_access_identity_path
-    }
+    domain_name              = var.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.thomaskimble_origin_access_control.id
+    origin_id                = local.s3_origin_id
   }
 
   enabled             = true
@@ -28,12 +25,13 @@ resource "aws_cloudfront_distribution" "thomaskimble_cloudfront_distribution" {
 
     forwarded_values {
       query_string = false
+
       cookies {
         forward = "none"
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
@@ -50,4 +48,11 @@ resource "aws_cloudfront_distribution" "thomaskimble_cloudfront_distribution" {
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2018"
   }
+}
+
+resource "aws_cloudfront_origin_access_control" "thomaskimble_origin_access_control" {
+  name                              = "thomaskimble origin access control"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
