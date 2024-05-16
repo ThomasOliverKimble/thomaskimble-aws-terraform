a44f9a90f147b8fa4817c110e95202efa25f1c37
ThomasOliverKimble <tokimble@gmail.com>
2024-05-16 02:51:08 +0100

    add options

--- api/main.tf
+++ api/main.tf
@@ -88,6 +88,74 @@ resource "aws_api_gateway_method_response" "mock_get_method_responses" {
 }
 
 
+# CORS OPTIONS method for each resource
+resource "aws_api_gateway_method" "options_methods" {
+  for_each      = aws_api_gateway_resource.mock_get_resources
+  rest_api_id   = aws_api_gateway_rest_api.thomaskimble.id
+  resource_id   = each.value.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.header.Origin"                         = false
+    "method.request.header.Access-Control-Request-Method"  = false
+    "method.request.header.Access-Control-Request-Headers" = false
+  }
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  for_each    = aws_api_gateway_method.options_methods
+  rest_api_id = aws_api_gateway_rest_api.thomaskimble.id
+  resource_id = each.value.resource_id
+  http_method = each.value.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = <<EOF
+      {
+        "statusCode": 200
+      }
+    EOF
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  for_each    = aws_api_gateway_method.options_methods
+  rest_api_id = aws_api_gateway_rest_api.thomaskimble.id
+  resource_id = each.value.resource_id
+  http_method = each.value.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
+resource "aws_api_gateway_method_response" "options_method_response" {
+  for_each    = aws_api_gateway_method.options_methods
+  rest_api_id = aws_api_gateway_rest_api.thomaskimble.id
+  resource_id = each.value.resource_id
+  http_method = each.value.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+
 # Deployment
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.thomaskimble.id
@@ -95,7 +163,9 @@ resource "aws_api_gateway_deployment" "api_deployment" {
     redeployment = sha1(jsonencode(concat(
       [aws_api_gateway_rest_api.thomaskimble.body, aws_api_gateway_rest_api.thomaskimble.root_resource_id],
       [for method in aws_api_gateway_method.mock_get_methods : method.id],
-      [for integration in aws_api_gateway_integration.mock_get_integrations : integration.id]
+      [for integration in aws_api_gateway_integration.mock_get_integrations : integration.id],
+      [for options_method in aws_api_gateway_method.options_methods : options_method.id],
+      [for options_integration in aws_api_gateway_integration.options_integration : options_integration.id]
     )))
   }
 
@@ -111,6 +181,7 @@ resource "aws_api_gateway_stage" "thomaskimble_prod" {
   deployment_id = aws_api_gateway_deployment.api_deployment.id
   stage_name    = "prod"
 }
+
 
 # Domain name
 resource "aws_api_gateway_domain_name" "thomaskimble_api_gateway_domain_name" {
