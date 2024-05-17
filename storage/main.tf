2ce7d35c093b7714d9cfb5ff48d2953e71e80386
ThomasOliverKimble <tokimble@gmail.com>
2024-05-17 15:04:52 +0100

    try this

--- storage/main.tf
+++ storage/main.tf
@@ -1,20 +1,7 @@
 locals {
   structure = yamldecode(file("${path.module}/file_structure.yaml"))
 
-  # Extract terminal paths with a fixed depth of 4
-  paths = toset(flatten([
-    for key1, value1 in local.structure : [
-      for key2, value2 in value1 : [
-        for key3, value3 in value2 : [
-          for key4, value4 in value3 :
-          "${key1}/${key2}/${key3}/${key4}"
-          if can(list(value3)) && length(value3) > 0
-        ]
-        if can(list(value2)) && length(value2) > 0
-      ]
-      if can(list(value1)) && length(value1) > 0
-    ]
-  ]))
+  paths = toset(keys(structure))
 }
 
 
